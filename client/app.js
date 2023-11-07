c7280ca26d17cdb7eb3d62bd1b58313b404cf95c
Leela Rani Aluru <76864260+LEELARANIALURU@users.noreply.github.com>
2023-11-07 08:37:06 -0800

    Add files via upload

---
+++ client/app.js
@@ -0,0 +1,29 @@
+
+function onClickedEstimateRisk() {
+
+    console.log("Estimate risk button clicked");
+    
+    var age = document.getElementById('age');
+    var sbp = document.getElementById("sbp");
+    var dbp = document.getElementById("dbp");
+    var sugar = document.getElementById("sugar");
+    var temp = document.getElementById("temp");
+    var heart = document.getElementById("heart");
+    var estRisk = document.getElementById("estRisk");
+    var url = "http://127.0.0.1:5000/predict_risk";
+  
+    $.post(url, {
+        age: parseInt(age.value),
+        sbp: parseInt(sbp.value),
+        dbp: parseInt(dbp.value),
+        sugar: parseInt(sugar.value),
+        temp: parseInt(temp.value),
+        heart: parseInt(heart.value),
+    },
+    
+    function(data, status) {
+        console.log(data.estRisk);
+        estRisk.innerHTML = "<h2>" + data.estRisk.toString();
+        console.log(status);
+    });
+  }
