e6657cf559162a93f43629c5bc2eeffb90fefedd
chandana444 <72591703+chandana444@users.noreply.github.com>
2024-04-12 00:09:19 +0530

    Add files via upload

---
+++ visceral/app.js
@@ -0,0 +1,9 @@
+function onClickedVisceralFat() {
+    console.log("Visceral fat button clicked");
+
+    var age = document.getElementById("age").value;
+    var estFat = document.getElementById("estFat");
+    var result = parseInt(age) * 2;
+    estFat.innerHTML = "<h2>" + result + "</h2>";
+}
+
